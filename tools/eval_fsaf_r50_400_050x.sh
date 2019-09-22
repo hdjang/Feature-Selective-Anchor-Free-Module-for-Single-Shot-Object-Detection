09952c32e1638a1bec8b26660a3e3b217ebf735b
hdjang <hdjang@kaist.ac.kr>
2019-09-22 21:05:40 +0000

    init

--- tools/eval_fsaf_r50_400_050x.sh
+++ tools/eval_fsaf_r50_400_050x.sh
@@ -2,7 +2,7 @@
 
 PYTHON=${PYTHON:-"python"}
 CONFIG=./configs/fsaf_r50_400_050x.py
-CKPT=./models/fsaf_r50_400_050x_epoch_1.pth
+CKPT=./models/fsaf_r50_400_050x.pth
 OUT=./models/results_fsaf_r50_400_050x.pkl
 
 CUDA_VISIBLE_DEVICES=1 $PYTHON $(dirname "$0")/test.py \
